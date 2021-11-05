af42f8c23ae83be4c70b1ddadfc0295a6d7e7b92
idrees khan <midreeskhan7276@gmail.com>
2021-11-05 09:30:03 -0700

    practice work

---
+++ app.js
@@ -0,0 +1,214 @@
+                //    CHAPTER = Array   //
+
+// Question  NO 1//
+
+// var stud = [ ];
+// stud[0]="jawan";
+// console.log(stud)
+
+// Question  NO 2//
+
+// var stud = [
+//     {Name : "idrees", Age : 21}
+//  ]
+// console.log(stud)
+
+// Question  NO 3//
+
+// var name1 =[
+//     "bilal","Idrees khan","Abdullah khan"
+// ]
+// console.log(name1)
+
+// Question  NO 4 //
+
+// var num1 =[
+//     41,52,150
+// ]
+// console.log(num1)
+ 
+// Question  NO 5 //
+
+// var user = prompt("enter city name")
+// var cites =["karachi","lahore","multan"]
+// if(user === cites[0]){
+//     document.write("welcome" )
+// }
+// else{document.write("sorry")}
+
+// Question  NO 6 //
+
+// nhi aya samaj
+
+// Question  NO 7 //
+ 
+
+// var arr = ["SSC", "HSC", "BCS",
+//     "BS", "BCOM", "MS", "M.Phil", "PhD"]
+//     document.write("1)" + arr[0]+ "<br>"+ "2)" + arr[1]+ "<br>"+ "3)" + arr[2]+ "<br>"
+//     + "4)" + arr[3]+ "<br>"+ "5)" + arr[4]+ "<br>"+ "6)" + arr[5]+ "<br>"
+//     + "7)" + arr[6]+ "<br>"+ "8)" + arr[7]+ "<br>")
+
+// Question  NO 8 //
+
+// var names =["bilal","Idrees","Abdullah"]
+// var score =[320,280,270]
+// document.write("score of "+names[0]+".percentage is :67%"+"<br>"+"score of "+names[1]+".percentage is :64%"+"<br>"+"score of "+names[2]+".percentage is :57%")
+  
+
+ // Question  NO 9 //
+
+// smjh ni araha
+
+
+// Question  NO 10//
+
+// var list =[850,250,200,522,250,500] 
+// document.write(list + "<br>"+ "<br>")
+// var list =[850,250,200,522,250,500]
+// list.sort()
+// document.write("updated " + list)
+
+
+
+    // Question  NO 11//
+
+
+// var cites =["karachi","lahore","multan" , "faislabd"]
+// document.write(cites+ " <br>" + " <br>" )
+// var cites =["karachi","lahore","multan" , "faislabd"]
+// var up = cites.slice(1,3)
+// document.write("updated = " + up)
+
+
+ // Question  NO 12 //
+
+//  var arr = [" this " , " is " , " my " ," cat "]
+//  document.write(arr + "<br>" + "<br>")
+
+// var arr = [" this " , " is " , " my " ," cat "]
+// var joi = arr.join(" " )
+// document.write("updated " + joi)
+
+
+// sir baki sawal samjh ni arahe
+
+  
+     ////// chpater = array & loop  ///////// 
+ 
+// Question  NO 1//
+
+// var arr = [
+//     [],
+//     []
+// ]
+
+// Question  NO 2 //
+
+
+// var arr = [
+// //     [0,1,2,3],
+// //     [1,0,1,2],
+// //     [2,1,0,1]
+
+// // ]
+// // for (var i = 0 ; i < 3 ; i++){
+// //     document.write(arr[i].join(" ") + "<br>")
+// // }
+
+// Question  NO 3 //
+
+// for (var i = 1 ; i <=10 ; i++)
+// {
+//     document.write(i +"<br>")
+// }
+
+// Question  NO 4//
+
+// var user =+prompt("enter ur table numbr")
+// var length =+prompt("enter ur length numbr")
+// for (var i= 1 ; i<=length ; i++ ){
+//     document.write(user + " x " + i + " = " + user * i + "<br>")
+// }
+
+// Question  NO 5 //
+
+// var arr =["apple" , "banana" ," mango " ," orange"  ]
+// for (var i= 0 ; i<arr.length ; i++ )
+// {
+//     document.write("Elemrnt at index "+i+ " is "  +arr[i] + "<br>")
+// }
+
+// Question  NO 6 //
+
+// for (var i = 1 ; i <=15 ; i++)
+// {
+//     document.write(i )
+// }
+
+// for (var i = 15 ; i >=1 ; i--)
+// {
+//     document.write(i +" ")
+// }
+
+
+
+// for (var i = 2 ; i <=20 ; i =i+2)
+// {
+//     document.write(i +" ")
+// }
+
+// for(var i = 1 ; i <=20 ; i =i+2)
+// {
+//     document.write(i +" ")
+// }
+
+// for (var i = 2 ; i <=20 ; i =i+2)
+// {
+//     document.write(i +"k ")
+// }
+
+// Question  NO 7 //
+
+// var user = prompt("enter item")
+// var isMatch= "no"
+// var items = ["cake" ,"coca cola " , "juice" , "biscuit"]
+//  for (var i = 0 ; i<items.length ; i++){
+//      if(user === items[i]){
+//          alert(user + " is available at index " + i +" in our bakery ")
+//     isMatch= "yes"
+//         }
+//  }if (isMatch == "no"){
+//      alert("not available")
+//  }
+
+// Question  NO  8 //
+
+
+// var arr =[10,100,300,60]
+// document.write(arr +"<br>")
+
+// var arr =[10,100,300,60]
+// var min = Math.min.apply(null, arr),
+//     max = Math.max.apply(null, arr);
+// document.write(max)
+
+
+// Question  NO 9//
+
+
+// var arr =[10,100,300,60]
+// document.write(arr +"<br>")
+
+// var arr =[10,100,300,60]
+// var min = Math.min.apply(null, arr)
+// document.write(min)
+
+
+// Question  NO 10 //
+
+// for (var i = 5 ; i <=100 ; i =i+5){
+        
+    
+// document.write(i +"<br>")
+// }
